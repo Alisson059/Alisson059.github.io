3a2ee867ba67a599a792f331832cb87f60bf8c13
Alisson059 <141637851+Alisson059@users.noreply.github.com>
2023-09-25 11:00:53 -0300

    Add files via upload

---
+++ profile/js/script.js
@@ -0,0 +1,11 @@
+function enviarMensagem() {
+    var nome = document.getElementById("idNome");
+
+    if (nome.value != "") {
+        alert("Obrigado, sr(a)" + nome.value + ", sua mensagem foi enviada com sucesso!");
+    }
+}
+
+
+
+
